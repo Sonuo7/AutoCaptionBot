9d75b5b6961170e6c17e99b90a61bb33fef2f592
Sonuo7 <rockysingh12ops@gmail.com>
2025-11-12 13:01:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "523894"))
-API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
-ADMINS = os.environ.get("ADMINS", "6167872503")
+API_ID = int(os.environ.get("API_ID", "28137469"))
+API_HASH = os.environ.get("API_HASH", "3b33568aa7119719d1f37cb15b3ae587")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7838294802:AAFJXUyRRKqhmR24j9NmRvYVudMD-ChQcw4")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @Alsamovies</b>")
+ADMINS = os.environ.get("ADMINS", "897584437")
