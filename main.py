665769648efe6799190867127d2eb9bbc75237ba
Sonuo7 <rockysingh12ops@gmail.com>
2025-11-12 13:25:19 +0530

    Clean up imports and initialize Client
    Removed unused imports and initialized the Client.

--- main.py
+++ main.py
@@ -67,12 +67,7 @@ class Bot(Client):
         print(f"{me.first_name} Bot stopped.")
 
 Bot().run()
-import asyncio
-import re
-from pyrogram import Client, filters, idle
-from pyrogram.errors import FloodWait, MessageNotModified
-from config import API_ID, API_HASH, BOT_TOKEN, CUSTOM_CAPTION, ADMINS
-
+#
 app = Client("autocaption-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 message_queue = []
